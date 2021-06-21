f0e3e2b6e5793fcc34fe2c1d1ce28bd9cd96f200
Matthew <mattjarman22@outlook.com>
2021-06-21 16:10:29 +0100

    Add progress animations

--- src/components/sections/skills.js
+++ src/components/sections/skills.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Icon from '../icons'
 import PropTypes from 'prop-types'
+import { useOnScreen } from '../../hooks'
+import { motion, useAnimation } from 'framer-motion'
 
 const Skills = ({ content }) => {
   const { frontmatter, exports } = content[0].node
   const { skills } = exports
 
+  const ref = useRef()
+  const onScreen = useOnScreen(ref, 0.5)
+
+  const barControls = useAnimation()
+
   const sortedSkills = skills.sort((a, b) => a.proficiency < b.proficiency)
+  const delay = 0.5 / sortedSkills.length
+
+  useEffect(() => {
+    if (onScreen) {
+      barControls.start(i => ({
+        opacity: 1,
+        width: '100%',
+        transition: { delay: i * delay },
+      }))
+    }
+  }, [onScreen, barControls])
 
   return (
     <section id="skills" className="flex flex-col mt-32 md:h-view">
@@ -26,11 +44,16 @@ const Skills = ({ content }) => {
                     <Icon className="w-6 h-6 xl:w-8 xl:h-8" name={icon} />
                   </div>
                   <div className="w-full bg-gray-100 dark:bg-gray-700 mr-4 shadow-lg border-2 border-green-400 rounded-md">
-                    <div
-                      style={{ width: `${proficiency}%` }}
-                      className="h-5 md:h-8 shadow-none flex flex-col font-bold px-2 text-gray-800 justify-center bg-green-400 text-xs md:text-sm"
-                    >
-                      {name}
+                    <div style={{ width: `${proficiency}%` }}>
+                      <motion.div
+                        custom={index}
+                        ref={ref}
+                        initial={{ opacity: 0, width: 0 }}
+                        animate={barControls}
+                        className="h-5 md:h-8 shadow-none flex flex-col font-bold px-2 text-gray-800 justify-center bg-green-400 text-xs md:text-sm"
+                      >
+                        {name}
+                      </motion.div>
                     </div>
                   </div>
                 </div>
@@ -47,7 +70,7 @@ Skills.propTypes = {
   content: PropTypes.arrayOf(
     PropTypes.shape({
       node: PropTypes.shape({
-        exports: PropTypes.string.isRequired,
+        exports: PropTypes.object.isRequired,
         frontmatter: PropTypes.object.isRequired,
       }).isRequired,
     }).isRequired
